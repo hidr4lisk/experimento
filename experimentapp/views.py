37add6092537b3a7155b7ebc105448511738bab8
Federico Furgiuele <federico.furgiuele@gmail.com>
2026-02-11 20:57:33 -0300

    Add .gitignore and remove venv from tracking

--- experimentapp/views.py
+++ experimentapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Página de inicio")
 
 # Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return HttpResponse("Login OK")
+        return HttpResponse("Credenciales inválidas", status=401)
+    return render(request, "experimentapp/login.html")  
