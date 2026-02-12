1d8efa90d6ef16d2d4191d3e734f08d0fe3b9f2b
Federico Furgiuele <federico.furgiuele@gmail.com>
2026-02-12 11:28:57 -0300

    Enable DEBUG mode to see error details

--- experimento/settings.py
+++ experimento/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-dev-key-change-in-production")
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,experimento-ikiy.onrender.com,.onrender.com").split(",")
 
